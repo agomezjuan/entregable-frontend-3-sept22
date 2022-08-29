7866f8e1286f451b207ae9998d3c8653db2f6704
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-08-29 14:18:07 +0200

    Envío de props a componentes Cabecera y Listado

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@
 // ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import React, { useEffect } from "react";
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
 
 function App() {
-
+  const [cantidad, setCantidad] = React.useState(0);
+  const incrementarCantidad = () => setCantidad(cantidad + 1);
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera cantidad={cantidad} />
+      <Listado incrementarCantidad={incrementarCantidad} />
     </div>
   );
 }
